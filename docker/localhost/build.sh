1c67a5158835e732560b514ec1b6e09328681309
Orion Letizi <oletizi@mac.com>
2019-03-18 15:48:50 -0700

    localhost docker image seems to work ok

--- docker/localhost/build.sh
+++ docker/localhost/build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 mydir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-
-
 cd ${mydir} \
   && . ./config.sh \
-  && docker build -f ./Dockerfile -t ${localhost_image} .
+  && docker build -f ./Dockerfile -t ${localhost_image} . \
+  && docker push ${localhost_image}
--- docker/localhost/run.sh
+++ docker/localhost/run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 mydir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+root=$( cd "${mydir}/../../" && pwd)
 
-cd ${mydir} \
-  && . config.sh \
-  && if [[ "" != $( docker ps | grep ${localhost_container} ) ]]; then
-    docker kill ${localhost_container} && docker rm ${localhost_container}
-  fi \
-  && docker run -d --name ${localhost_container} ${localhost_image}
+cd ${mydir}
+. config.sh 
+docker pull ${localhost_image} 
+docker kill ${localhost_container}
+docker rm ${localhost_container}
+docker run -d -v ${root}:/home/ubuntu/avi-k8s-test --name ${localhost_container} ${localhost_image}
