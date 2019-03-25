67cfdac10d1bb65b7c40500e39fd669532c028e6
Orion Letizi <oletizi@mac.com>
2019-03-25 14:11:59 -0700

    gcloud installation on alpine seems to work

--- bin/01-install-gcloud.sh
+++ bin/01-install-gcloud.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-cd /tmp/
-curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-239.0.0-linux-x86_64.tar.gz | tar xfvz -
-/tmp/google-cloud-sdk/install.sh
 gcloud init
