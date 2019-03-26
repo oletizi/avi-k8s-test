3e64fd50d243ef9d5f0a88ce8d23d35246a715b7
Orion Letizi <oletizi@mac.com>
2019-03-25 18:38:15 -0700

    added zone to cluster delete in cleaunup script

--- bin/100-cleanup.sh
+++ bin/100-cleanup.sh
@@ -9,7 +9,7 @@ echo "Cleaning up. This may take a few minutes..."
 if [[ "" != $(gcloud container clusters list | grep ${AVI_DEMO_CLUSTER_NAME}) ]]; then
     echo "Deleting cluster ${AVI_DEMO_CLUSTER_NAME}..."
     echo
-    gcloud container clusters delete ${AVI_DEMO_CLUSTER_NAME}
+    gcloud container clusters delete --zone=${AVI_DEMO_CONTROLLER_INSTANCE_ZONE} ${AVI_DEMO_CLUSTER_NAME}
 fi
 
 if [[ "" != $(gcloud compute instances list | grep ${AVI_DEMO_CONTROLLER_INSTANCE_NAME}) ]]; then
