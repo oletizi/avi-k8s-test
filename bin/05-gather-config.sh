fd84650e4ea970f117cc766e4c360253c8d65306
Orion Letizi <oletizi@mac.com>
2019-03-27 16:45:27 -0700

    generate an admin password on the fly

--- bin/05-gather-config.sh
+++ bin/05-gather-config.sh
@@ -7,7 +7,10 @@ root=$( cd "${mydir}/.." && pwd )
 # gather ssh keys for gcloud compute resources and add them to ssh config
 gcloud compute config-ssh
 
-# TODO: Test with a non-static IP
+# Generate a password to use for the controller
+password=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
+
+# Get the controller host ip
 echo "Fetching Avi controller host IP address..."
 controller_ip=$(gcloud compute instances describe --zone=${AVI_DEMO_CONTROLLER_INSTANCE_ZONE} ${AVI_DEMO_CONTROLLER_INSTANCE_NAME} --format=\"json\" | jq -r '.networkInterfaces[0].accessConfigs[0].natIP')
 echo "Avi controller host IP address: ${controller_ip}"
@@ -21,8 +24,10 @@ project_name=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
 echo "Current project name: ${project_name}"
 
 # Write transient config to configuration file
+# XXX: This could be prettier
 echo "Writing transient configuration to config file: ${AVI_DEMO_CONFIG}..."
 echo "export AVI_DEMO_MASTER_IP=${master_ip}" > ${AVI_DEMO_CONFIG}
+echo "export AVI_DEMO_CONTROLLER_PASSWORD='${passwd}'" >> ${AVI_DEMO_CONFIG}
 echo "export AVI_DEMO_CONTROLLER_IP=${controller_ip}" >> ${AVI_DEMO_CONFIG}
 echo "export AVI_DEMO_CONTROLLER_HOSTNAME=${AVI_DEMO_CONTROLLER_INSTANCE_NAME}.${AVI_DEMO_CONTROLLER_INSTANCE_ZONE}.${AVI_DEMO_PROJECT}" >> ${AVI_DEMO_CONFIG}
 echo "export AVI_DEMO_PROJECT_NAME=${project_name}" >> ${AVI_DEMO_CONFIG}
--- bin/08-config-controller.sh
+++ bin/08-config-controller.sh
@@ -12,6 +12,6 @@ default_avi_password=$1
 playbook=${HOME}/devops/ansible/gcp/controller_kcloud.yaml
 #playbook=${ansible}/controller_kcloud.yaml
 
-cmd="ansible-playbook -i ${AVI_DEMO_ANSIBLE_HOSTS_FILE}  ${playbook} -vvv -e \"password=1CoolNewPassword controller_ip=${AVI_DEMO_CONTROLLER_IP} avi_config_state=present default_avi_password=${default_avi_password} kube_master_node=${AVI_DEMO_MASTER_IP} project_name=${AVI_DEMO_PROJECT_NAME}\""
+cmd="ansible-playbook -i ${AVI_DEMO_ANSIBLE_HOSTS_FILE}  ${playbook} -vvv -e \"password=${AVI_DEMO_CONTROLLER_PASSWORD} controller_ip=${AVI_DEMO_CONTROLLER_IP} avi_config_state=present default_avi_password=1CoolNewPassword kube_master_node=${AVI_DEMO_MASTER_IP} project_name=${AVI_DEMO_PROJECT_NAME}\""
 echo "Executing ${cmd}"
 eval ${cmd}
