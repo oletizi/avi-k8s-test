5f6f510fbcf975cf5dbbb936d414e7a40653ff46
Orion Letizi <oletizi@mac.com>
2019-04-16 14:33:53 -0700

    removed version tagging now done by bump-version.sh; added docker push

--- sbin/release.sh
+++ sbin/release.sh
@@ -4,10 +4,14 @@ set -exv
 
 mydir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 root=$( cd "${mydir}/.." && pwd )
+docker=$( cd "${root}/docker/avi-k8s-demo-localhost" && pwd )
 version_file="${root}/VERSION"
 
 . "${mydir}/config.sh"
 . "${root}/bin/config.sh"
+. "${docker}/config.sh"
+
+assert_not_empty "Localhost docker image name not defined!" ${localhost_image}
 
 # ensure we're up to date
 git pull \
@@ -16,10 +20,8 @@ git pull \
   && assert_not_empty "Failed to determine version!" ${version} \
   && echo "version: ${version}" \
   && "${mydir}"/build.sh \
-  && git add -A \
-  && git commit -m "version $version" \
-  && git tag -a "$version" -m "version $version" \
   && git push \
-  && git push --tags
+  && git push --tags \
+  && docker push ${localhost_image}:${version}
 assert_success $? "Release failed!"
 
